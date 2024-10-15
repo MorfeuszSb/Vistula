978a4a1de07d223dbe8b423d6459537a432bfc38
MorfeuszSb <sbryl2@stu.vistula.edu.pl>
2024-10-15 19:53:49 +0200

    Update7

--- zadanie3/src/Main.java
+++ zadanie3/src/Main.java
@@ -3,8 +3,9 @@ public class Main
 {
     public static void main(String[] args)
     {
+        // wyswietlenie komunikatu na ekranie
         System.out.println("Hello Vistula!");
-// to koniec programu
+        // to koniec programu
     }
 }
 
