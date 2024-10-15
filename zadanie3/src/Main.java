87c9127e5b4a747fbb1e76f66f9aed3476cc3f30
MorfeuszSb <sbryl2@stu.vistula.edu.pl>
2024-10-15 19:53:49 +0200

    Update7

--- zadanie3/src/Main.java
+++ zadanie3/src/Main.java
@@ -1,11 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        // Tworzenie obiektu Account
+        Account myAccount = new Account("Jan Kowalski", 1000, "123456789");
 
-public class Main
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Hello Vistula!");
-// to koniec programu
+        try {
+            myAccount.transfer(1500);
+        } catch (NotEnoughMoneyException e) {
+            System.out.println("Błąd: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Wystąpił inny błąd: " + e.getMessage());
+        } finally {
+            System.out.println("Aktualny stan konta: " + myAccount.getBalance() + " zł.");
+        }
     }
 }
-
-
